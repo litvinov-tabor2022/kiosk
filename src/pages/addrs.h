e55438d9805b20f3a90a8ef16ee21c211856fef1
Jenda Kolena <jendakolena@gmail.com>
2022-07-12 22:10:47 +0200

    fixup! Adapt to new UI, update framework to 0.10.1, minor tweaks
    fixing UI

--- src/pages/addrs.h
+++ src/pages/addrs.h
@@ -79,7 +79,6 @@ public:
             static const u16 NextButton = 0x1012;
 
             static const u16 VpAddrBase = 0x1100;
-            static const u16 SpAddrBase = 0x2100;
             // other skill addrs for texts are derived: Base + row * 0x0100 + col * 0x0030
         } s;
     } u;
